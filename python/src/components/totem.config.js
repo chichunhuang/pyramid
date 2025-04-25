4261256271b3abd51eec62f53951fa430957a586
chichun <chichunhuamg@gmail.com>
2025-04-25 16:43:17 +0800

    pyramid: gen docs-map

---
+++ python/src/components/totem.config.js
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) insect-totem.net
+ *
+ * This source code is licensed
+ */
+
+const constants = { 
+
+homePage: {  latestTitle: '最新異動 Fri Apr 25 16:41:31 CST 2025',  latestItems: [ { key: 1, url: '/docs/IDE/PyCharm_HotKeys', content: 'PyCharm 中的快速鍵 - PyCharm 中的快速鍵' },  { key: 2, url: '/docs/Python入門/Python_Basic', content: 'Python 快速回憶 - Python 學習所需基本操作' },  { key: 3, url: '/docs/Python入門/Python_Typing_String', content: 'Python String 字串 - Python String' },  { key: 4, url: '/docs/Python基礎操作/Python_Convention_Assignment', content: 'Python 變數指派 - Python Variable Assignment' },  { key: 5, url: '/docs/Python基礎操作/Python_Convention', content: 'Python 慣例 - Python 變數命名慣例' },  { key: 6, url: '/docs/Python基礎操作/Python_2_Function', content: 'Python 函數 - Python 函數介紹' },  { key: 7, url: '/docs/Python特殊操作/Python_2_Generator', content: 'Python Generator - Python Generator - lazy loop' },  { key: 8, url: '/docs/Python類別與OOP/Python_4_Site', content: 'Python OOP Site Map - Python OOP Site Map' },  { key: 9, url: '/docs/Python類別與OOP/Python_4_Inheritance_Basic', content: 'Python 繼承介紹 - Python Inheritance' },  { key: 10, url: '/docs/Python類別與OOP/Python_4_AbstractMethod', content: 'Python 抽象方法 - Python Abstract Method' }, ], },  };
+
+module.exports = constants;
